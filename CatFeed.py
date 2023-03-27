5f701332eeb77a5cac7d8af1fc5081bb92516822
TwhomeGH <agp05432@gmail.com>
2023-03-27 09:35:46 +0800

    類優化

--- CatFeed.py
+++ CatFeed.py
@@ -7,41 +7,39 @@ class GetItem:
 
     def __init__(self):
         """初始化"""
+        self.Hour=0
         self.Play=0
-        self.xp=0
-        self.xpCount=0
-        self.remReadxp=0
-        self.feed=0
-        self.feedCount=0
+        self.xp={'Count':0,'Value':0}
+        self.feed={'Count':0,'Value':0}
         self.CC=0
         self.Rest=False
-        self.Hour=0
-
+        
 
     def __str__(self) -> str:
-        return self.xp
+        return f'已探險{self.Play}次 共{self.Hour}小時 '
+    
 
 
     def AddXP(self,value):
         """
         增加xp次數&給定xp
         """  
-        self.xpCount+=value
-        self.xp+=1
+        self.xp['Value']+=value
+        self.xp['Count']+=1
 
 
     def AddFeed(self,value):
         """增加罐頭次數&給定罐頭"""
-        self.feedCount+=value
-        self.feed+=1
+        self.feed['Value']+=value
+        self.feed['Count']+=1
 
 
     def Range(self):
         """獲得統計概率 回傳為字典"""
-        All=self.xp+self.feed
+        All=self.xp['Count']+self.feed['Count']
         if All==0:return None
-        XPRange=round((self.xp/All)*100)
-        FeedRange=round((self.feed/All)*100)
+        XPRange=round((self.xp['Count']/All)*100)
+        FeedRange=round((self.feed['Count']/All)*100)
 
         return {'XP':XPRange,'Feed':FeedRange,'Count':All}
 
@@ -50,16 +48,14 @@ class GetItem:
         """獲得統計數量"""
         return f"""
         已探險{self.Play}
-        獲得 xp:{self.xp}次 +{self.xpCount} 貓罐頭:{self.feed}次
-        xpRemRead 得到:{self.remReadxp}
-        元寶 得到:{self.CC}個
-        共{self.Hour}小時
+        獲得 xp:{self.xp['Count']}次 +{self.xp['Value']} 貓罐頭:{self.feed['Count']}次 +{self.feed['Value']}
+        共{self.Hour}小時 元寶 得到:{self.CC}個
         """
     
 
     def ResultH(self):
         """獲得統計數量(只取xp&貓罐頭) 和次數"""
-        return f"獲得 xp:{self.xp}次 +{self.xpCount} 貓罐頭:{self.feed}次 已探險{self.Play}次 共{self.Hour}小時"
+        return f"獲得 xp:{self.xp['Count']}次 +{self.xp['Value']} 貓罐頭:{self.feed['Count']}次 +{self.feed['Value']} 已探險{self.Play}次 共{self.Hour}小時"
 
 
 # Item=GetItem()
